38a716ecc6e7df60344bf9e39043275ee4bcd9db
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-04-30 01:47:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+$(".services-button").click(function(){
+  $(".nav-content").fadeOut();
+  $(".foot").fadeOut();
+  $("#services ").fadeIn(1500);
+});
+
+
+$(".contact-button").click(function(){
+  $("#services").fadeOut();
+  $(".nav-content").fadeOut();
+  $(".foot").fadeIn(1500);
+});
+
+
+
+
+
+$(".navbar-brand").click(function(){
+  $(".nav-content").fadeIn(1500);
+  $(".foot").fadeIn();
+  $("#services").fadeIn();
+});
+
+
+
+  $(".dropdown-industries").mouseenter(function(){
+    $(".dropdown-for-industries").fadeIn(800); 
+  });
+
+  $(".dropdown-for-industries, .dropdown-industries").mouseleave(function(){
+    $(".dropdown-for-industries").hide(); 
+  });
+
+
+  $(".dropdown-company").mouseenter(function(){
+    $(".dropdown-for-company").fadeIn(800); 
+  });
+
+  $(".dropdown-for-company, .dropdown-company").mouseleave(function(){
+    $(".dropdown-for-company").hide(); 
+  });
